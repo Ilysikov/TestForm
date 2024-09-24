714155102b1bc772dda3b8b555957eac6227a980
Иван Лысиков <ivan.lysikov@fojin.tech>
2024-09-24 19:38:02 +0300

    PageFactory

--- tests/main.py
+++ tests/main.py
@@ -2,12 +2,16 @@ import os
 import shutil
 from pathlib import Path
 
+import pytest
+
 
 def clean():
-    path=Path("/Users/ivanlysikov/PycharmProjects/TestForm/tests/allure-results")
+    path = Path("/Users/ivanlysikov/PycharmProjects/TestForm/tests/allure-results")
     for i in path.glob("**/*"):
-        if i.name!="history":
+        if i.name != "history":
             i.unlink()
+
+
 def add_history():
     path2 = Path("/Users/ivanlysikov/PycharmProjects/TestForm/tests/allure-report/history")
     path3 = Path('/Users/ivanlysikov/PycharmProjects/TestForm/tests/allure-results/history')
@@ -17,20 +21,18 @@ def add_history():
         shutil.copy(
             os.path.join(path2, n),
             os.path.join(path3))
-import pytest
+
+
 if __name__ == "__main__":
-    # pytest.main(["test_index_page.py::TestFooter::test_first"])
-    # pytest.main(["-n 2"])
-    # pytest.main(["pytest --alluredir allure-results"])
-    # pytest.main(["test_index_page.py::TestForm::test_email"])
-    # pytest.main(["test_index_page.py::TestFooter::test_last"])
-    # pytest.main(["--dist= -durations=5"])
     clean()
     os.system("pytest --alluredir allure-results")
     add_history()
     os.system("allure serve allure-results")
 
-
-
-
+    # pytest.main(["test_index_page.py::TestFooter::test_first"])
+    # pytest.main(["-n 5"])
+    # pytest.main(["pytest --alluredir allure-results"])
+    # pytest.main(["test_index_page.py::TestForm::test_email"])
+    # pytest.main(["test_index_page.py::TestFooter::test_last"])
+    # pytest.main(["--dist= -durations=5"])
     # pytest.main(["test_index_page.py::TestFooter::test_open","test_index_page.py::TestFooter::test_hobbies"])
